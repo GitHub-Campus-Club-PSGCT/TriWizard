867a422e3b8a590bb2e195bcef2d17f3bd9e5c85
Subha Subbiah <subha@Subhas-MacBook-Air.local>
2025-08-17 11:43:20 +0530

    /verify route complete

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,8 +1,33 @@
 const express = require("express");
 const router = express.Router();
+const Team = require("../models/Team");
 
-router.get("/", (req, res) => {
-  res.json({ message: "Login route works" });
+router.post("/", async (req, res) => {
+  try {
+    const { teamName, otp } = req.body;
+    console.log("Received:", { teamName, otp, parsedOtp: parseInt(otp) });
+
+    //find team by name
+    const team = await Team.findOne({ teamName: teamName });
+
+    console.log("Query result:", team);
+
+    //no team found
+    if (!team) {
+      return res.status(401).json({ success: false, message: "Team not found" });
+    }
+
+    //Check OTP
+    if (team.otp === parseInt(otp)) {
+      return res.json({ success: true, message: "Login successful", team });
+    } else {
+      return res.status(401).json({ success: false, message: "Invalid OTP" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Server error" });
+  }
 });
+
 
 module.exports = router;
