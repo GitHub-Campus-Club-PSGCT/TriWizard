74a602273bc5581111ec702278ca8935e928a82d
Subha Subbiah <subhasubbiah7@gmail.com>
2025-08-17 23:17:28 +0530

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,14 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const adminRoutes = require("./admin");
-//const emailVerificationRoutes = require("./emailVerification.js");
 const loginRoutes = require("./login");
-//const otpRoutes = require("./otp");
 
 //mount routes
 router.use("/admin", adminRoutes);
-//router.use("/email-verification", emailVerificationRoutes);
 router.use("/login", loginRoutes);
-//router.use("/otp", otpRoutes);
 
 module.exports = router;
