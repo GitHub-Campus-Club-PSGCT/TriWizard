342a7753ff8c9f37ae088d9ac7792888df26f209
Teju Beast <sundarapuram6@gmail.com>
2025-08-27 00:04:06 +0530

    console half rdy

--- frontend/participants/src/App.jsx
+++ frontend/participants/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 
 import Login from './components/login/login';
-
+import WizardIDE from './components/login/console';
 
 import './App.css';
 
@@ -12,6 +12,7 @@ function App() {
       <AuthProvider>
         <Routes>
           <Route path="/login" element={<Login />} />
+          <Route path="/ide" element={<WizardIDE />} />
         </Routes>
       </AuthProvider>
     </Router>
