5dda58202d8404b555964765ded5b82dc92bde65
Teju Beast <sundarapuram6@gmail.com>
2025-08-30 22:25:35 +0530

    rules added

--- frontend/participants/src/App.jsx
+++ frontend/participants/src/App.jsx
@@ -4,6 +4,7 @@ import { AuthProvider } from './context/AuthContext';
 import Login from './components/login/login';
 import WizardIDE from './components/login/console';
 import Leaderboard from './components/leaderboard';
+import Rules from './components/greathall';
 import './App.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/ide/:housename/:questionNumber" element={<WizardIDE />} />
           <Route path="/ld" element={<Leaderboard />} />
+          <Route path="/rules" element={<Rules />} />
         </Routes>
       </AuthProvider>
     </Router>
